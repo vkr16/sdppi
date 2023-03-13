9f269862c1a34881611023b7fe5071c962a21339
Fikri Miftah A <fikri.droid16@gmail.com>
2023-03-13 10:22:18 +0700

    remove premium plugin of tinymce from lists

--- app/Views/admin/informasi.php
+++ app/Views/admin/informasi.php
@@ -25,9 +25,9 @@
         tinymce.init({
             selector: 'textarea#content',
             plugins: [
-                'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'export',
+                'autolink',
                 'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
-                'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
+                'fullscreen', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
             ],
             toolbar: 'undo redo | a11ycheck casechange blocks | bold italic backcolor | alignleft aligncenter alignright alignjustify |' +
                 'bullist numlist checklist outdent indent | removeformat | code table help'
